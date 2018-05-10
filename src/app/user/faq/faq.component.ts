0c3fb64fc757bc359cab424543d22d7ca67deed1
Tier5 LLC <work@tier5.us>
2018-05-10 19:23:31 +0530

    faq frontend v1.3

--- src/app/user/faq/faq.component.ts
+++ src/app/user/faq/faq.component.ts
@@ -1,115 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-//import {CommonModule} from '@angular/common';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { FaqService } from '../services/faq.service';
 
 
 @Component({
-  selector: 'app-faq',
-  templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.css','../../../custom-inner.css']
+  selector      : 'app-faq',
+  templateUrl   : './faq.component.html',
+  styleUrls     : ['./faq.component.css','../../../custom-inner.css']
 })
 export class FaqComponent implements OnInit {
-  faqDetails    : any[] = [];
-  errorMessage  : string;
-  faqData       : any[] = [];
-  counter       : number;
-  // name : string;
-  // faqCount: number;
-  // //public modalRef: BsModalRef;
-  // faqData: any[] = [];
-  // delfaqId: number;
-  // delfaqStatus: string;
-  // delfaqStatusMsg: string = 'Are You Sure?';
-constructor(
-    private faqService: FaqService,
-    //private modalService: BsModalService,
+    faqDetails    : any[] = [];
+    errorMessage  : string;
+    faqData       : any[] = [];
+    counter       : number; // a common counter used to index the categories
+    innerCounter  : number; // a common counter used to index the faq data per categories
+    constructor(
+        private faqService: FaqService,
+    ) { }
 
-) { }
-
-ngOnInit() {
-  // console.log(this.faqData);
-  this.getFaqCategories();
-  
-}
-
-getFaqCategories() {
-  this.faqService.getFaqCategories().subscribe(
-      (data: any) => {
-          this.faqData      = data.data;
-          this.faqDetails   = this.getQuestions(this.faqData,0,null);
-          console.log('faq data',this.faqData);
-          console.log('faq faq details',this.faqDetails);
-      },
-      (error: any) => {
-        console.log(error);
-        for(let prop in error.error.message){
-          this.errorMessage = error.error.message[prop];
-          console.log(this.errorMessage);
-          break;
-        };
-        //console.log(this.errorMessage);
-        setTimeout(() => {
-          this.errorMessage = '';
-        },3000)
-      }
-  );
-}
-
-getQuestions(faqEachData : any[], cnt : number, event : Event) : any {
-    if(event !== null) {
-        console.log(event);
+    ngOnInit() {
+        this.getFaqCategories();
     }
-    console.log('faq each data',faqEachData,faqEachData[cnt]['faq-details'],' event : ',event);
-    this.counter    = cnt;
-    this.faqDetails = faqEachData[cnt]['faq-details'] !== undefined ? faqEachData[cnt]['faq-details'] : [];
-    return this.faqDetails;
-}
 
-showAnswers(faqDetailsData) {
-    console.log(faqDetailsData);
-}
+    /* *
+    *   Get data object from api
+    *   slice that in 2 parts : categories as faqData , q & a from categories as faqDetails
+    * */
+    getFaqCategories() {
+        this.faqService.getFaqCategories().subscribe(
+            (data: any) => {
+                this.faqData      = data.data;
+                this.faqDetails   = this.getQuestions(this.faqData,0);
+            },
+            (error: any) => {
+                console.log(error);
+                for(let prop in error.error.message){
+                    this.errorMessage = error.error.message[prop];
+                    console.log(this.errorMessage);
+                    break;
+                };
+                setTimeout(() => {
+                    this.errorMessage = '';
+                },3000)
+            }
+        );
+    }
 
-hideAnswers(faqDetailsData) {
-    console.log(faqDetailsData);
-}
+    /* *
+    *   Get questions from the data object
+    * */
+    getQuestions(faqEachData : any[], count : number) : any {
+        this.counter        = count;
+        this.innerCounter   = null;
+        this.faqDetails     = faqEachData[count]['faq-details'] !== undefined ? faqEachData[count]['faq-details'] : [];
+        return this.faqDetails;
+    }
 
-getFaqs() {
-    // this.faqService.faqList().subscribe(
-    //     (data: any) => {
-    //         // console.log(data.data);
-    //         this.faqList = data.data.faqDetails;
-    //         this.faqCount = this.faqList.length;
-    //     }
-    // );
-}
-
-  //public openModal(template:  TemplateRef<any>, index) {
-      // console.log(index);
-      // this.modalRef = this.modalService.show(template);
-      // this.faqData = this.faqList[index];
-      // this.delfaqId = this.faqList[index].id;
-  //}
-
-  onCancel() {
-      // this.modalRef.hide();
-      // this.delfaqId = null;
-  }
-
-  onitemDel() {
-      // this.faqService.deletefaq(this.delfaqId).subscribe(
-      //     (data: any) => {
-      //         this.delfaqStatus = data.status;
-      //         if (this.delfaqStatus) {
-      //             let itemModalRef = this;
-      //             itemModalRef.delfaqStatusMsg = data.message;
-      //             setTimeout(function() {
-      //                 itemModalRef.modalRef.hide();
-      //                 this.delfaqId = null;
-      //             }, 2000);
-      //             this.getFaqs();
-      //         }
-      //     }
-      // );
-  }
+    /* *
+    *   Hide or show the answer for a question from a single
+    *   question based on their related states
+    * */
+    showOrHideAnswers(faqDetailsData : any, index : number) {
+        this.innerCounter = this.innerCounter == index ? null : index;
+    }
 }
