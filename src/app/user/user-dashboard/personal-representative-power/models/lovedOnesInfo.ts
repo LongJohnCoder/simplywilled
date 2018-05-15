505bbb44e1ad0ae771bc773367ae3a808084db7a
Tier511 <work@tier5.us>
2018-05-16 00:10:38 +0530

    personal representative power value set and forward to next step v0.1

---
+++ src/app/user/user-dashboard/personal-representative-power/models/lovedOnesInfo.ts
@@ -0,0 +1,19 @@
+export class LovedOnesInfo {
+  business_interest: string;
+  compensation_percent_amount: number;
+  compensation_specific_amount: number;
+  created_at_resp: string;
+  deleted_at_resp: string;
+  farm_or_ranch: string;
+  id: number;
+  is_complete: string;
+  is_getcompensate: string;
+  is_percentage: string;
+  is_tangible_property_distribute: string;
+  net_value_percent: string;
+  residue_to_partner_first: string;
+  specific_gifts: string;
+  tangible_property_distribute: any;
+  updated_at: string;
+  user_id_resp: number;
+}
--- src/app/user/user-dashboard/personal-representative-power/personal-representative-power.component.ts
+++ src/app/user/user-dashboard/personal-representative-power/personal-representative-power.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {PersonalRepresentativePowerService} from './services/personal-representative-power.service';
 import {Observable} from 'rxjs/Observable';
-
+import {UserService} from '../../user.service';
+import {LovedOnesInfo} from './models/lovedOnesInfo';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-personal-representative-power',
   templateUrl: './personal-representative-power.component.html',
@@ -10,7 +12,7 @@ import {Observable} from 'rxjs/Observable';
 })
 export class PersonalRepresentativePowerComponent implements OnInit {
 
-  constructor(private prService: PersonalRepresentativePowerService) { }
+  constructor(private prService: PersonalRepresentativePowerService, private usrService: UserService, private router: Router) { }
   businessInterestClassFlag: boolean;
   farmRanchFlag: boolean;
   personalRepresentativeFlag: boolean;
@@ -20,26 +22,99 @@ export class PersonalRepresentativePowerComponent implements OnInit {
   errFlag: boolean;
   errString: string;
   savePersonalRepresentativeDB: Observable<any>;
+  fetchPersonalRepresentativeDB: any;
+  session_user_id: number;
+  fetchedUserData: LovedOnesInfo;
+  fetchInfo: boolean;
+  compensation_value_data: any;
   ngOnInit() {
-    this.businessInterestClassFlag = false;
-    this.farmRanchFlag = false;
-    this.personalRepresentativeFlag = false;
-    this.amountTypeFlag = false;
-    this.isNetValueFlag = false;
+    this.getStepDetails();
+    this.fetchInfo = false;
     if (JSON.parse(localStorage.getItem('loggedInUser')).hasOwnProperty('token')) {
       this.access_token = JSON.parse(localStorage.getItem('loggedInUser')).token;
     } else {
       this.access_token = '';
     }
+
   }
+
+  /**
+   * get step user details
+   */
+  getStepDetails(): void {
+    this.session_user_id = JSON.parse(localStorage.getItem('loggedInUser')).user.id;
+    if (this.session_user_id) {
+      this.fetchPersonalRepresentativeDB = this.usrService.getUserDetails(this.session_user_id);
+      this.fetchPersonalRepresentativeDB.subscribe(data => {
+        if (data.status && data.status === 200) {
+          if (data.data.hasOwnProperty(3)) {
+            if (data.data[3].hasOwnProperty('data') && data.data[3].data) {
+              this.fetchedUserData = data.data[3].data.lovedOnesInfo[0];
+            } else {
+              console.log('No records found. Please fill up the form');
+              this.errFlag = true;
+              this.errString = 'No records found. Please fill up the form';
+            }
+          } else {
+            this.errFlag = true;
+            this.errString = 'No array index exists for this step';
+          }
+        } else {
+          this.errFlag = true;
+          this.errString = 'Error while fetching records';
+        }
+      }, error => {
+        this.errFlag = true;
+        this.errString = error.error.message;
+      }, () => {
+        this.fetchInfo = true;
+        // set dynamic values to the form
+        if (this.fetchedUserData.business_interest === '1') {
+          this.businessInterestClassFlag = true;
+        } else {
+          this.businessInterestClassFlag = false;
+        }
+        if (this.fetchedUserData.farm_or_ranch === '1') {
+          this.farmRanchFlag = true;
+        } else {
+          this.farmRanchFlag = false;
+        }
+        if (this.fetchedUserData.is_getcompensate === '1') {
+          this.personalRepresentativeFlag = true;
+        } else {
+          this.personalRepresentativeFlag  = false;
+        }
+        if (this.fetchedUserData.is_percentage !== '1') {
+          this.amountTypeFlag = false;
+          this.compensation_value_data = this.fetchedUserData.compensation_percent_amount;
+        } else {
+          this.amountTypeFlag  = true;
+          this.compensation_value_data = this.fetchedUserData.compensation_specific_amount;
+        }
+        if (this.fetchedUserData.net_value_percent === '1') {
+          this.isNetValueFlag = true;
+        } else {
+          this.isNetValueFlag  = false;
+        }
+      });
+    } else {
+      this.errFlag = true;
+      this.errString = 'We are unable to log you in. Please login to continue';
+    }
+  }
+  /**
+   * this function update the record in database
+   * @param {NgForm} formData
+   * @returns {any}
+   */
   savePersonalRepresentative(formData: NgForm): any {
     if (this.access_token.length) {
-      const dataset = {'step': 4, 'user_id': JSON.parse(localStorage.getItem('loggedInUser')).user.id, 'lovedOnesInfo': [{'user_id': JSON.parse(localStorage.getItem('loggedInUser')).user.id, 'business_interest': formData.value.business_interest ? '1' : '0', 'farm_or_ranch': formData.value.farm_or_ranch ? '1' : '0', 'is_percentage': formData.value.percentage_estate ? '1' : '0', 'is_getcompensate': formData.value.percentage_compensation ? '1' : '0' , 'compensation_specific_amount' : formData.value.percentage_estate ? formData.value.compensation_value : 0 , 'compensation_percent_amount' : !formData.value.percentage_estate ? formData.value.compensation_value : 0, 'net_value_percent' : formData.value.net_value ? '1' : '0'}]};
+      const dataset = {'step': 4, 'user_id': JSON.parse(localStorage.getItem('loggedInUser')).user.id, 'lovedOnesInfo': [{'user_id': JSON.parse(localStorage.getItem('loggedInUser')).user.id, 'business_interest': formData.value.business_interest_val ? '1' : '0', 'farm_or_ranch': formData.value.farm_or_ranch_val ? '1' : '0', 'is_percentage': formData.value.percentage_estate_val ? '1' : '0', 'is_getcompensate': formData.value.percentage_compensation_val ? '1' : '0' , 'compensation_specific_amount' : formData.value.percentage_estate_val ? formData.value.compensation_value_val : 0 , 'compensation_percent_amount' : !formData.value.percentage_estate_val ? formData.value.compensation_value_val : 0, 'net_value_percent' : formData.value.net_value_val ? '1' : '0'}]};
       this.savePersonalRepresentativeDB = this.prService.savePersonalRepresentativePower(this.access_token, dataset);
       this.savePersonalRepresentativeDB.subscribe(data => {
         if (data.status) {
           // router link to next page
-          console.log('updated');
+          this.router.navigate(['/personal-representative-details\n']);
         } else {
           this.errFlag = true;
           this.errString = 'Something went wrong while updating the dataset. Please try again later!';
