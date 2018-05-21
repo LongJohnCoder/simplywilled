b6a04a3f5bfc5477f8ba315c08e569487c6f9663
tier5 <work@tier5.us>
2018-05-21 15:20:41 +0530

    forget password page ui v0.5

---
+++ src/app/user/user-auth/forget-password/forget-password-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+
+const routes: Routes = [{path: '', component: ForgetPasswordComponent}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ForgetPasswordRoutingModule { }
---
+++ src/app/user/user-auth/forget-password/forget-password.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ForgetPasswordRoutingModule } from './forget-password-routing.module';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ForgetPasswordRoutingModule
+  ],
+  declarations: [ForgetPasswordComponent]
+})
+export class ForgetPasswordModule { }
