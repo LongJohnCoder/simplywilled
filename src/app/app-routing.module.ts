c2ca6d9c2a4ccfd4ff5758b3c0efc2fcae334f1e
Tier5 LLC <github@tier5.us>
2018-05-30 16:13:43 +0530

    UI changes V0.1 stable released to dev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const appRoutes: Routes = [
     { path: 'admin-panel', loadChildren: './admin/admin.module#AdminModule' },
     { path: '', loadChildren: './user/user.module#UserModule'},
+    { path: '**', redirectTo: ''}
 ];
 
 @NgModule({
