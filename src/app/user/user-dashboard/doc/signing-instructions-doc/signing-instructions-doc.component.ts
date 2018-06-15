1eb6ab2e9df8ce352b68daecaa13ccb4f2689d64
tier5 <work@tier5.us>
2018-06-16 02:27:58 +0530

    Dashboard document preview page UI v0.4

--- src/app/user/user-dashboard/doc/signing-instructions-doc/signing-instructions-doc.component.ts
+++ src/app/user/user-dashboard/doc/signing-instructions-doc/signing-instructions-doc.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import * as html2canvas from "html2canvas";
-import * as jsPdf from 'jspdf';
+//import * as jsPdf from 'jspdf';
 import {UserService} from "../../../user.service";
 import {Subscription} from "rxjs/Subscription";
 import {UserAuthService} from "../../../user-auth/user-auth.service";
+import {ProgressbarService} from "../../shared/services/progressbar.service";
 //import 'jspdf-autotable' as JA from 'jspdf-autotable';
 
 @Component({
@@ -14,6 +15,9 @@ import {UserAuthService} from "../../../user-auth/user-auth.service";
 export class SigningInstructionsDocComponent implements OnInit, OnDestroy {
   @ViewChild('docBox')
   docBox: any;
+  @ViewChild('thumbContainer')
+  thumbContainer: any;
+  progressSubscription: Subscription;
   thumbIndex: number;
   scrollHeight: number;
   docScrolled: number;
@@ -25,24 +29,72 @@ export class SigningInstructionsDocComponent implements OnInit, OnDestroy {
     '../../../../../assets/images/doc1-thumb1.png',
     '../../../../../assets/images/doc1-thumb2.png'
   ];
-  loggedInUser: any;
-
+  liCount: number;
+  progressBar = {
+    finalArrangements: false,
+    healthFinance: false,
+    protectYourFinance: false,
+    provideYourLovedOnes: false,
+    tellUsAboutYou: false
+  };
+  loggedInUser:any;
   getUserDetailsSubscription: Subscription;
-  constructor(private userService: UserService, private userAuth: UserAuthService) {
+  count: number;
+  constructor(private userService: UserService, private userAuth: UserAuthService,private progressbarService: ProgressbarService,) {
     this.loggedInUser = this.userAuth.getUser();
     this.getUserDetails();
+    let token = JSON.parse(localStorage.getItem('loggedInUser')).token;
+    this.progressSubscription = this.progressbarService.fetchTotalCompletion(token).subscribe(
+      (progress: any) => {
+        if (progress.status !== undefined) {
+          this.progressBar = {
+            finalArrangements: progress.data !== null && progress.data.final_arrangements !== undefined && progress.data.final_arrangements,
+            healthFinance: progress.data !== null && progress.data.health_finance !== undefined && progress.data.health_finance,
+            protectYourFinance: progress.data !== null && progress.data.protect_your_finance !== undefined && progress.data.protect_your_finance,
+            provideYourLovedOnes: progress.data !== null && progress.data.provide_your_loved_ones !== undefined && progress.data.provide_your_loved_ones,
+            tellUsAboutYou: progress.data !== null && progress.data.tell_us_about_you !== undefined && progress.data.tell_us_about_you
+          };
+          this.count = this.setProgress(this.progressBar, 0);
+        } else {
+          console.log('Oops, something went wrong with the progress bar.');
+        }
+      },
+      (error) => { console.log(error); },
+      () => {}
+    );
+  }
+
+  /**Sets the progress count**/
+  setProgress(progressBar: Object, count = 0) {
+    if (this.progressBar.finalArrangements) {
+      count++;
+    }
+    if (this.progressBar.healthFinance) {
+      count++;
+    }
+    if (this.progressBar.protectYourFinance) {
+      count++;
+    }
+    if (this.progressBar.provideYourLovedOnes) {
+      count++;
+    }
+    if (this.progressBar.tellUsAboutYou) {
+      count++;
+    }
+    return count;
   }
 
   ngOnInit() {
-    //this.x = 1;
     this.docScrolled = 0;
     this.thumbIndex = 0;
+    this.liCount = this.docThumbImg.length * 114;
   }
 
   scrollDoc(index:number){
     this.scrollHeight = 991 * index;
     this.docBox.nativeElement.scrollTop = this.scrollHeight;
     this.thumbIndex = index;
+    //this.thumbContainer.nativeElement.scrollLeft(100);
     //this.docBox.nativeElement.style.transition = 'top .8s cubic-bezier(0.77, 0, 0.175, 1)';
   }
 
@@ -52,6 +104,10 @@ export class SigningInstructionsDocComponent implements OnInit, OnDestroy {
     }else{
       this.thumbIndex = 0;
     }
+    if(this.thumbIndex >= 4){
+      this.thumbContainer.nativeElement.scrollLeft = this.thumbIndex * 31;
+    }
+
   }
 
   /**Get the user details*/
