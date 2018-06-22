b6c2caf424695432b61d13b7e3a98bec894cac65
Tier5 LLC <github@tier5.us>
2018-06-23 04:11:56 +0530

    fix directive issues

--- src/app/user/user-dashboard/doc/prevent-copy.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PreventCopyDirective } from './prevent-copy.directive';
-
-describe('PreventCopyDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PreventCopyDirective();
-    expect(directive).toBeTruthy();
-  });
-});
