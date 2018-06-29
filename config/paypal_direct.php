7b053c877bee47ec9c3a6e83bb503b055e08482d
tier5 <work@tier5.us>
2018-06-30 01:29:54 +0530

    Interview Changes Done v1.0 #stable

--- config/paypal_direct.php
+++ config/paypal_direct.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-  'signature' => 'A.XOJFbCOgWS3AGK6A0Equv15VTdABTLXw6hw.Dp-RbewQjPR2PN8l9i',
-  'user' => 'ace1_api1.tier5.in',
-  'password' => 'CT3R2WD8GRSE8C2A',
+  'signature' => 'APHhTBfLdUY0OrALGtFdgv8c0MxqAx81g1XNeFdl1L2QPsW21wQER8NV',
+  'user' => 'simply-merchant_api1.simplywilled.com',
+  'password' => '6ECB3NR5NV6PMUKX',
   'host' => 'https://api-3t.sandbox.paypal.com/nvp',
   'method' => 'DoDirectPayment',
   'paymentAction' => 'Sale'
