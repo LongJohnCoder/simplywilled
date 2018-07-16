20c339343fd67423ee6128557df842732c08edcd
Tier5 LLC <work@tier5.us>
2018-07-16 20:54:01 +0530

    final sprint v7.4

--- src/app/user/user-dashboard/doc/healthcare-poa-doc/ky/ky.component.ts
+++ src/app/user/user-dashboard/doc/healthcare-poa-doc/ky/ky.component.ts
@@ -1,3 +1,4 @@
+import { GlobalPdfService } from './../../services/global-pdf.service';
 import {Component, Input, OnChanges, OnInit} from '@angular/core';
 
 @Component({
@@ -24,10 +25,21 @@ export class KyComponent implements OnInit, OnChanges {
     tellUsAboutYou: null
   };
   loading = true;
-
-  constructor() { }
+  totalPages: number;
+  constructor(
+    private globalService: GlobalPdfService
+  ) { }
 
   ngOnInit() {
+  }
+
+  setThNails() {
+    const x = this.globalService.getDynamicPages();
+    this.totalPages = x.totalPages;
+    this.globalService.hcpoaPages({
+      'pages' : x.totalPages,
+      'heightArr' : x.heightArr
+    });
   }
 
   ngOnChanges() {
@@ -48,6 +60,7 @@ export class KyComponent implements OnInit, OnChanges {
       };
       this.loading = false;
       console.log(this.userDetails);
+      this.setThNails();
     }
   }
 
