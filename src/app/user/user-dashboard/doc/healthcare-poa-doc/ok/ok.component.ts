48507dcf232a1bb39f4f751e2957a1a01413eb2b
Tier5 LLC <work@tier5.us>
2018-07-16 23:23:18 +0530

    final sprint v7.8

--- src/app/user/user-dashboard/doc/healthcare-poa-doc/ok/ok.component.ts
+++ src/app/user/user-dashboard/doc/healthcare-poa-doc/ok/ok.component.ts
@@ -1,3 +1,4 @@
+import { GlobalPdfService } from './../../services/global-pdf.service';
 import {Component, Input, OnChanges, OnInit} from '@angular/core';
 
 @Component({
@@ -26,11 +27,22 @@ export class OkComponent implements OnInit, OnChanges {
   };
   loading = true;
 
-  constructor() { }
+  totalPages: number;
+  constructor(
+    private globalService: GlobalPdfService
+  ) { }
 
   ngOnInit() {
   }
 
+  setThNails() {
+    const x = this.globalService.getDynamicPages();
+    this.totalPages = x.totalPages;
+    this.globalService.hcpoaPages({
+      'pages' : x.totalPages,
+      'heightArr' : x.heightArr
+    });
+  }
   ngOnChanges() {
     if (this.data !== undefined && this.data !== null) {
       this.userDetails = {
@@ -49,6 +61,7 @@ export class OkComponent implements OnInit, OnChanges {
       };
       this.loading = false;
       console.log(this.userDetails);
+      this.setThNails();
     }
   }
 }
