d1632542df6b1c76447c94520ff34f360f3ea4d1
Tier5 <work@tier5.us>
2018-07-19 20:47:11 +0530

    tour guide ui v1.1

--- src/app/user/user-dashboard/global-tour/global-tour.component.ts
+++ src/app/user/user-dashboard/global-tour/global-tour.component.ts
@@ -32,17 +32,17 @@ export class GlobalTourComponent implements OnInit, OnDestroy {
 
   // tslint:disable-next-line:member-ordering
   guide = {
-    1 : 'This is your dashboard, from anywhere you click here to view the over all status of your five documents',
-    2 : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quam quibusdam eius necessitatibus eos totam laborios',
-    3 : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quam quibusdam eius necessitatibus eos totam laborios',
-    4 : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quam quibusdam eius necessitatibus eos totam laborios',
-    5 : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quam quibusdam eius necessitatibus eos totam laborios',
-    6 : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quam quibusdam eius necessitatibus eos totam laborios',
-    7 : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quam quibusdam eius necessitatibus eos totam laborios',
-    8 : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quam quibusdam eius necessitatibus eos totam laborios',
-    9 : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quam quibusdam eius necessitatibus eos totam laborios',
-    10: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quam quibusdam eius necessitatibus eos totam laborios',
-    11: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quam quibusdam eius necessitatibus eos totam laborios'
+    1 : 'This is your dashboard, from anywhere you click here to view the over all status of your five documents.',
+    2 : 'This is your first document, Tell us About You. Our owl will help you through each steps in the five documents when you have any questions. You just need to explore yourself!',
+    3 : 'Once you provide your necessary information for us to prepare your documents, you can check the documents in this section.',
+    4 : 'By clicking this you you preview the next document.',
+    5 : 'By clicking this you you preview the previous document.',
+    6 : 'By clicking this you can download your document.',
+    7 : 'By clicking this a mail will be sent from our end to your personal email, with the PDF version of the current document.',
+    8 : 'By clicking this you can print your document.',
+    9 : 'Want to refer a friend to Simply Willed? Simple just provide the name of your friend with his/her email addess!',
+    10: 'You can customise your account here!',
+    11: 'Finally the logout button. Now you are all set to explore simplywilled and prepare your documents and testaments. Voila!'
   };
 
   changeTourState(type: string) {
@@ -55,7 +55,9 @@ export class GlobalTourComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    PageScrollConfig.defaultScrollOffset = this.stepNumber === 9 ? 400 : 250;
+    console.log('tourguide: ', this.document.getElementById("tourguide").offsetTop, 'window scroll: ', window.scrollY);
+    PageScrollConfig.defaultScrollOffset = this.stepNumber === 9 ? 450 : 250;
+    // PageScrollConfig.defaultScrollOffset = this.stepNumber === 5 ? 400 : 250;
     console.log('step num : ', this.stepNumber);
     if (this.stepNumber < 1 || this.stepNumber > 11) {
       this.changeTourState('close');
