2e50fe7e1eb1cd0ab82786411e942efa3b219afb
Tier5 <work@tier5.us>
2018-07-20 23:24:42 +0530

    error message ui v2.0

--- src/app/user/user-dashboard/global-tour/global-tour.component.ts
+++ src/app/user/user-dashboard/global-tour/global-tour.component.ts
@@ -32,17 +32,17 @@ export class GlobalTourComponent implements OnInit, OnDestroy {
 
   // tslint:disable-next-line:member-ordering
   guide = {
-    1 : 'This is your dashboard, from anywhere you click here to view the over all status of your five documents.',
-    2 : 'This is your first document, Tell us About You. Our owl will help you through each steps in the five documents when you have any questions. You just need to explore yourself!',
-    3 : 'Once you provide your necessary information for us to prepare your documents, you can check the documents in this section.',
-    4 : 'By clicking this you you preview the next document.',
-    5 : 'By clicking this you you preview the previous document.',
-    6 : 'By clicking this you can download your document.',
-    7 : 'By clicking this a mail will be sent from our end to your personal email, with the PDF version of the current document.',
-    8 : 'By clicking this you can print your document.',
-    9 : 'Want to refer a friend to Simply Willed? Simple just provide the name of your friend with his/her email addess!',
-    10: 'You can customise your account here!',
-    11: 'Finally the logout button. Now you are all set to explore simplywilled and prepare your documents and testaments. Voila!'
+    1 : 'By clicking Back To Dashboard, you will be sent back to the main dashboard, where you can view the stages of your interview. From the dashboard you can see which stages are complete, in progress, or need to be completed.',
+    2 : 'Need some additional information during the interview? Not to worry! Just click on my icon next to an interview question to see additional helpful estate planning tips and suggestions.',
+    3 : 'The Preview Documents button allows you to preview your document at any time during the interview. Once you have completed the interview, you will be able to email, download, and print your completed documents.',
+    4 : 'Click the Next Document button to review the next document in your estate plan package.',
+    5 : 'If you think you skipped something or just want to read your document again, you can always go back to your previous document and review it.',
+    6 : 'Once you review your document and completed all your interview questions, you can download each documents as PDF. Remember you need to download each document individually.',
+    7 : 'Clicking the Email button will send you an email with the selected document right to your inbox. Remember you need to email each document individually.',
+    8 : 'The Print button allow you to print your documents directly from SimplyWilled.com, and you can sign them right away. Remember you need to print each document individually.',
+    9 : 'Enjoy using SimplyWilled.com and think someone else would benefit? Refer a friend to SimplyWilled.com so they can get their affairs in order. ',
+    10: 'The My Account button is where you can change your password and your personal information.',
+    11: 'Whenever you log out, don’t worry! Your data is automatically saved. Wherever you stop, you can start again at the same place!'
   };
 
   changeTourState(type: string) {
